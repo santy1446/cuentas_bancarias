dd6bb66407bc875532e58e177f708c60ab6cae71
santy1446 <santy1446@gmail.com>
2020-04-11 15:48:03 -0500

    merge

--- cuentas_bancarias/urls.py
+++ cuentas_bancarias/urls.py
@@ -1,6 +1,8 @@
 
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from rest_framework.authtoken import views
 from cuentas.views import AccountAPI, StateAccountAPI, TypeAccountAPI, BalanceAPI, MovementAPI, CategoryAPI
 
@@ -18,3 +20,5 @@ urlpatterns = [
     path('auth-token/', views.obtain_auth_token)
 
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
